a8c115afa3a6afccda6aa92dd2943fb267e7e81b
Alexandre Lorthiois <lorthioisalexandre3@gmail.com>
2026-05-13 14:52:20 +0100

    improvements

--- dbt/mollie/analyses/unit_economics_summary.sql
+++ dbt/mollie/analyses/unit_economics_summary.sql
@@ -1,7 +1,7 @@
 -- Average CAC across the period per channel
 SELECT
     acquisition_channel,
-    ROUND(AVG(cac), 0)                  AS avg_cac,
+	10000*6/sum(new_customers_registered) as avg_cac,
     ROUND(AVG(avg_month_0_revenue), 2)  AS avg_month_0_revenue
 FROM fct_customer_acquisition
 GROUP BY 1
